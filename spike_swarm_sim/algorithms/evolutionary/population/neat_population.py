5e8096023e6f203f43a9d6662f13cb5ed0bf7e1f
Rafael Sendra <r.sendra@alumnos.upm.es>
2021-03-02 19:32:53 +0100

    No crossover when len(species) == 1

--- spike_swarm_sim/algorithms/evolutionary/population/neat_population.py
+++ spike_swarm_sim/algorithms/evolutionary/population/neat_population.py
@@ -55,6 +55,8 @@ class NEAT_Population(Population):
         species_offsprings = []
         for spc in self.species:
             spc_fitness, spc_genotypes = zip(*filter(lambda x: x[1]['species'] == spc.id, zip(fitness_vector, self.population)))
+            if len(spc_genotypes) == 1: # If only one genotype in species, no crossover.
+                offspring.append(spc_genotypes[0])
             num_offspring = max(2, int(np.round(self.pop_size * sum(spc_fitness) / sum(fitness_vector))))
             num_offspring = int(0.6 * len(spc_genotypes) + 0.4 * num_offspring) if np.abs(num_offspring - len(spc_genotypes)) > 0 else len(spc_genotypes)
             species_offsprings.append(num_offspring)
@@ -69,6 +71,7 @@ class NEAT_Population(Population):
         for n_offspring, spc in zip(species_offsprings, self.species):
             #! OJO DEEPCOPY????
             spc_fitness, spc_genotypes = zip(*filter(lambda x: x[1]['species'] == spc.id, zip(fitness_vector, self.population)))
+            import pdb; pdb.set_trace()
             #* Truncate bests
             n_sel = max(2, int(0.4 * len(spc_genotypes))) #! Truncate only 40% best. Note that implem is diff from GA!
             parents, fitness_parents = truncation_selection(spc_genotypes, np.array(spc_fitness), n_sel)
