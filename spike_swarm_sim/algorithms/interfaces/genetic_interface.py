629f3d3055f0b93f15485e43996ad0f6e7a3d2c3
Rafael Sendra <r.sendra@alumnos.upm.es>
2021-03-15 11:01:23 +0100

    Fixed minimal typo bug in NEAT interface.

--- spike_swarm_sim/algorithms/interfaces/genetic_interface.py
+++ spike_swarm_sim/algorithms/interfaces/genetic_interface.py
@@ -145,7 +145,7 @@ class NEATInterface:
         for key in genotype['connections']:
             if key not in self.neural_net.graph['synapses']:
                 syn = genotype['connections'][key]
-                self.neural_net.add_synapse(key, syn['pre'], syn['post'], syn['wei\ght'], conn_prob=1.)
+                self.neural_net.add_synapse(key, syn['pre'], syn['post'], syn['weight'], conn_prob=1.)
             self.neural_net.graph['synapses'].update({key : genotype['connections'][key].copy()})
 
         #* Update parameters (Decoders and encoders not supported yet).
