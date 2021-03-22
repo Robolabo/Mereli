2935b1ed8ef17b04a4ecb8a3b4aa266026613011
Rafael Sendra <r.sendra@alumnos.upm.es>
2021-03-22 09:40:16 +0100

    NEAT adaptive species thresh. (x2)

--- spike_swarm_sim/neural_networks/neural_net.py
+++ spike_swarm_sim/neural_networks/neural_net.py
@@ -290,7 +290,6 @@ class NeuralNetwork:
             actions [dict]: dict mapping output names and actions.
         ===============================================================
         """
-        import pdb; pdb.set_trace()
         #* --- Convert stimuli into spikes (Encoders Step) ---
         if len(stimuli) == 0:
             raise Exception(logging.error('The ANN received empty stimuli.'))
