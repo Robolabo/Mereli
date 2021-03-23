d651446f73d05066c8c19be30209e25bc9745c46
Rafael Sendra <r.sendra@alumnos.upm.es>
2021-03-23 19:29:18 +0100

    Checkpoint updated.

--- spike_swarm_sim/objectives/fitness.py
+++ spike_swarm_sim/objectives/fitness.py
@@ -191,7 +191,8 @@ class TwoLights:
 
             distances_green = LA.norm(pos[:, :2] - green_light_pos[:2], axis=1)
             distances_yellow = LA.norm(pos[:, :2] - yellow_light_pos[:2], axis=1)
-            fA = 0.5 * (distances_green < 1).mean() + 0.5 * (distances_yellow < 1).mean()
+            fA = 0.5 * any(distances_green < 1) + 0.5 * any(distances_yellow < 1)
+
             # fA = {
             #     0 : 0.5*(distances_green < 1).mean() + 0.5*(distances_yellow < 1).mean(),
             #     1 : sum(distances_green < 1) ,
