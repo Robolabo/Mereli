38d3a7216dd5cfa20c5da3b8fd8e23ae9281b2c3
Rafael Sendra <r.sendra@alumnos.upm.es>
2021-04-10 19:18:05 +0200

    Minor changes.

--- spike_swarm_sim/objectives/fitness.py
+++ spike_swarm_sim/objectives/fitness.py
@@ -157,10 +157,10 @@ class GotoLight:
             #* Distance of every robot to the nearest neighbor
             distances_robots = np.array([np.min([LA.norm(pos_i - pos_j) for j, pos_j in enumerate(pos) if i != j]) 
                                 for i, pos_i in enumerate(pos)])
-            fA = (distances < 1).mean()
+            fA = (distances < 1.5).mean() * (np.sum(distances < 1.5) > 1)
             # fB = np.mean(distances_robots > 0.4)
             # fC = np.mean(distances_robots < 1.5)
-            # import pdb; pdb.set_trace()
+            # import pdb; pdb.set_trace()g
             fitness += fA# * fB * fC
         return (fitness / len(states)) + 1e-5
 
