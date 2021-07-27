0ccbbe22ebdc155175c2a08cba06ac046da95ff1
Rafael Sendra <r.sendra@alumnos.upm.es>
2021-07-27 14:14:36 +0200

    World API created, setup.py, world API example.

--- build/lib/spike_swarm_sim/objects/__init__.py
+++
@@ -1,7 +0,0 @@
-from .world_object import WorldObject2D, WorldObject3D
-from .light_source import LightSource, FoodArea, Nest, LightSource3D
-from .robot import Robot
-from .robot_3D import Robot3D
-from .wall import Wall, Wall2D
-from .ball import Ball
-from .task_scheduler import TaskScheduler
