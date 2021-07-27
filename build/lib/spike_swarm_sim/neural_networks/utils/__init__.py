b2658035d670ac215accc33e9d596ece6babb16c
Rafael Sendra <r.sendra@alumnos.upm.es>
2021-07-27 14:20:24 +0200

    Minor changes

--- build/lib/spike_swarm_sim/neural_networks/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-from .monitor import NeuralNetMonitor
-from .builder import  SynapsesBuilder
-
-try:
-    from .visualization import  *
-except:
-    logging.warning('Visualization Module cannot be loaded. Running without it')
-from .utils import  *
