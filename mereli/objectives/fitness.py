1a523378347a5a90931465b5fb4c7c5a441184e8
Rafael Sendra <r.sendra@alumnos.upm.es>
2022-02-11 11:43:43 +0100

    Minor changes

--- mereli/objectives/fitness.py
+++ mereli/objectives/fitness.py
@@ -276,7 +276,10 @@ class TaskSwitchingB:
     def __call__(self, actions, states, info=None):
         fitness = 0
         for t, rew_t in enumerate(info['robot:reward']):
-            V_t = np.sum([np.clip(info['robot:reward'][k], a_min=0, a_max=1) * 0.95 ** (k-t) for k in range(t, len(states))],0)
+            try:
+                V_t = np.sum([np.clip(info['robot:reward'][k].flatten(), a_min=0, a_max=1) * 0.95 ** (k-t) for k in range(t, len(states))],0)
+            except:
+                import pdb; pdb.set_trace()
             # F_tA = len(rew_t) / np.sum((np.clip(V_t, a_min=0, a_max=None)+1e-5)**-1)
             # F_tB = np.prod(V_t) ** (1 / len(V_t))
             fitness += np.mean(V_t)
