2b746b5022e32c592feaf4149e51528145553007
Rafael Sendra <r.sendra@alumnos.upm.es>
2022-02-26 21:52:01 +0100

    Minor changes

--- mereli/objectives/fitness.py
+++ mereli/objectives/fitness.py
@@ -281,7 +281,7 @@ class TaskSwitchingB:
             # except:
             #     import pdb; pdb.set_trace()
             # F_tA = len(rew_t) / np.sum((np.clip(rew_t.flatten(), a_min=0, a_max=None)+1e-5)**-1)
-            if sum(rew_t.flatten() > 0) > len(rew_t)//2:
+            if 1:#sum(rew_t.flatten() > 0) > len(rew_t)//2:
                 if t <= len(states) / 2: 
                     fA += np.mean(rew_t.flatten())
                 else:
