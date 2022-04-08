Normalization bug fixed

mereli/neural_networks/neural_net.py
@@ -121,6 +121,8 @@ class NeuralNetwork(BaseNeuralNet):
             actions [dict]: dict mapping output names and actions.
         ===============================================================
         """
+        if np.max(np.abs(self.weights)) > 10:
+            import pdb; pdb.set_trace()
         task = stimuli['task']
         # stimuli['distance_sensor'] *= 0.
         #* --- Convert stimuli into spikes (Encoders Step) ---
@@ -132,7 +134,7 @@ class NeuralNetwork(BaseNeuralNet):
         self.stimuli = stimuli.copy()
         if self.time_scale == 1:
             inputs = inputs[np.newaxis]
-        
+
         #* --- Apply update rules to synapses ---
         if self.learning_rule is not None and self.t > 1:
             # If reward is None  while learning rule is not, then 
@@ -153,7 +155,7 @@ class NeuralNetwork(BaseNeuralNet):
         actions = self.decoders.step(spikes_window[:, self.motor_neurons])
         self.prev_input = inputs[-1].copy()
         #* --- Debugging stuff (DEBUG MODE) --- #
-        if self.t == self.time_scale * 1999 and self.monitor is not None:
+        if self.t == self.time_scale * 3999 and self.monitor is not None:
             oo = np.stack(tuple(self.monitor.get('outputs').values()))
             ii = np.stack(tuple(self.monitor.get('stimuli').values()))
             II = np.stack(tuple(self.monitor.get('currents').values()))

mereli/physics_engines/pybullet_engine.py
@@ -221,7 +221,7 @@ class PybulletEngine(BaseEngine):
         """
         pos = np.array(p.getBasePositionAndOrientation(identifier, physicsClientId=self.client)[0])        
         if np.isnan(pos).any(): 
-            print(pos)
+            print(pos, self.__dict__)
             import pdb; pdb.set_trace()
         return pos
     

mereli/utils/normalization.py
@@ -19,7 +19,7 @@ class LinearNormalization(Normalization):
         super(LinearNormalization, self).__init__(*args, **kwargs)
 
     def apply(self, value):
-        return (value - self.min_val) / (self.max_val - self.min_val)
+        return np.clip((value - self.min_val) / (self.max_val - self.min_val), a_min=0, a_max=1)
 
     def revert(self, value):
         return value * (self.max_val - self.min_val) + self.min_val
@@ -30,7 +30,7 @@ class ExpNormalization(Normalization):
         super(ExpNormalization, self).__init__(*args, **kwargs)
 
     def apply(self, value):
-        return (np.log10(0.5 * value) - self.min_val) / (self.max_val - self.min_val)
+        return np.clip((np.log10(0.5 * value) - self.min_val) / (self.max_val - self.min_val), a_min=0, a_max=1)
 
     def revert(self, value):
         return 2 * 10 ** (value * (self.max_val - self.min_val) + self.min_val)