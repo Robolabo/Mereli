27dee6a15415483acb2683a7fc38bbedda31f038
r-sendra <r.sendra@alumnos.upm.es>
2022-08-07 11:15:49 +0200

    Minor reward bug fixed x2

--- mereli/tasks/task.py
+++ mereli/tasks/task.py
@@ -111,7 +111,6 @@ class TaskAllocation(Task):
         all_led = np.array([int(ent.actuators['led_actuator'].action[0])\
             for ent in entities.values() if issubclass(type(ent), Robot)])
         if all(led != led2 for led in all_led for led2 in all_led):
-            __import__('pdb').set_trace()
             return np.array([5.])
         if not any(led == robot_led for led in others_led):
             #return np.array([1])
