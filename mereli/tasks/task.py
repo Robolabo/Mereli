Bug fixed in reward computation.

mereli/tasks/task.py
@@ -147,11 +147,11 @@ class CommFormation(Task):
         my_state = entities[robot_name].sensors['stateful_rx'].state
         others_state = np.array([ent.sensors['stateful_rx'].state\
             for ent in entities.values() if issubclass(type(ent), Robot) and ent.id != entities[robot_name].id])
-        closest = np.argmin([np.linalg.norm(pt - my_state) for pt in self.points]) 
+        closest = self.points[np.argmin([np.linalg.norm(pt - my_state) for pt in self.points])] 
         alpha = 2
         r1 = np.exp(-alpha * np.linalg.norm(my_state - closest)) 
         r2 = np.min([np.linalg.norm(oth_st - my_state) for oth_st in others_state]) / np.sqrt(8) 
-        r2 = np.exp(5*r2 - 5)
+        r2 = np.exp(5 * r2 - 5)
         reward = r1 * r2 
         return reward
 