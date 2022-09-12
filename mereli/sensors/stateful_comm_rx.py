adeff4967bf284c5d82c9513b64d7209002ba9a5
r-sendra <r.sendra@alumnos.upm.es>
2022-09-12 11:12:10 +0200

    Now ann can know the diff vector to a target point.

--- mereli/sensors/stateful_comm_rx.py
+++ mereli/sensors/stateful_comm_rx.py
@@ -56,9 +56,13 @@ class StatefulCommRX(Sensor):
         state_agg = neigh_mean
         self.state = own_state
         closest_state = state_diffs[np.argmin([np.linalg.norm(st_df) for st_df in state_diffs])] 
-        return {'mean_neigh_state' : state_agg + np.random.randn(self.state_dim) * 0.05,
-                'closest_state' : closest_state  + np.random.randn(self.state_dim) * 0.05,
-                'own_state' : own_state + np.random.randn() * 0.05}
+        target_points = np.array([[-1, -1], [1, 1], [-1, 1], [1, -1], [0, 0]])
+        closest_tar = target_points[np.argmin([np.linalg.norm(pt - self.state) for pt in target_points])] 
+        __import__('pdb').set_trace()
+        return {'mean_neigh_state' : state_agg,# + np.random.randn(self.state_dim) * 0.05,
+                'closest_state' : closest_state,#  + np.random.randn(self.state_dim) * 0.05,
+                'closest_target' : closest_tar - own_state, 
+                'own_state' : own_state}#+ np.random.randn() * 0.05}
 
 
 @sensor_registry(name='comm_rx_a')
