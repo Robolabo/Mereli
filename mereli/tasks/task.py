e7db02de6e2bcc2a3e87455021a9ab54fb1bd505
r-sendra <r.sendra@alumnos.upm.es>
2022-09-20 12:13:45 +0200

    Minor changes

--- mereli/tasks/task.py
+++ mereli/tasks/task.py
@@ -148,20 +148,20 @@ class CommFormation(Task):
         my_state = entities[robot_name].sensors['stateful_rx'].state
         others_state = np.array([ent.sensors['stateful_rx'].state\
             for ent in entities.values() if issubclass(type(ent), Robot) and ent.id != entities[robot_name].id])
-        # closest = self.points[np.argmin([np.linalg.norm(pt - my_state) for pt in self.points])] 
-        free_spots = [*filter(lambda pt: all([np.linalg.norm(st - pt) > self.threshold for st in others_state]), self.points)]
-        closest = free_spots[np.argmin([np.linalg.norm(pt - my_state) for pt in free_spots])] 
+        closest = self.points[np.argmin([np.linalg.norm(pt - my_state) for pt in self.points])] 
+        # free_spots = [*filter(lambda pt: all([np.linalg.norm(st - pt) > self.threshold for st in others_state]), self.points)]
+        # closest = free_spots[np.argmin([np.linalg.norm(pt - my_state) for pt in free_spots])] 
         num_closest = np.sum([np.linalg.norm(st - closest) < self.threshold for st in others_state]) #Thresh before 0.2 
         inside_area = np.linalg.norm(my_state - closest) < self.threshold 
 
-        alpha = 30
+        alpha = 10
         # if inside_area:
         r1 = np.exp(-alpha * np.linalg.norm(my_state - closest)) 
         # r2 = np.min([np.linalg.norm(oth_st - my_state) for oth_st in others_state]) / np.sqrt(8) 
         if inside_area and num_closest == 0:
             return r1
-        elif inside_area and num_closest > 0:
-            return -r1
+        else:
+            return r1*r2
         return 0.0 
     
     def reward_generator2(self, entities, robot_name):
