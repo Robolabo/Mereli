54d00011d3dd4f461de4032d2b6716af29c6a5f1
r-sendra <r.sendra@alumnos.upm.es>
2022-10-06 10:26:07 +0200

    Minor changes

--- mereli/sensors/stateful_comm_rx.py
+++ mereli/sensors/stateful_comm_rx.py
@@ -107,18 +107,19 @@ class OrientStatefulCommRX(Sensor):
         closest_state = state_diffs[np.argmin([np.linalg.norm(st_df) for st_df in state_diffs])] 
         target_points = np.array([[.75, .75], [-.75, -.75], [-.75, .75], [.75, -.75]])
         closest_tar = target_points[np.argmin([np.linalg.norm(pt - own_state) for pt in target_points])] - self.state
-        phi_closest_st = np.arccos(closest_state.dot(heading_vec) / (np.linalg.norm(closest_state) * np.linalg.norm(heading_vec)))
-        phi_closest_tar = np.arccos(closest_tar.dot(heading_vec) / (np.linalg.norm(closest_tar) * np.linalg.norm(heading_vec)))
-
+        phi_closest_st = np.arccos(closest_state.dot(heading_vec) / np.linalg.norm(closest_state))
+        phi_closest_tar = np.arccos(closest_tar.dot(heading_vec) / np.linalg.norm(closest_tar))
+        dist_closest_st =  np.linalg.norm(closest_state) 
+        dist_closest_tar = np.linalg.norm(closest_tar)
         inside_area = np.linalg.norm(closest_tar - own_state) < 0.4
         area_full = np.sum([np.linalg.norm(st - closest_tar) < 0.4 for st in neigh_state]) > 3
         return {'mean_neigh_state' : state_agg,# + np.random.randn(self.state_dim) * 0.05,
                 'closest_state' : closest_state,#  + np.random.randn(self.state_dim) * 0.05,
                 'closest_target' : closest_tar - own_state, 
-                'phi_closest_st' : np.array([phi_closest_st / (2*np.pi)]),
-                'phi_closest_tar' : np.array([phi_closest_tar / (2*np.pi)]),
-                'dist_closest_tar' : np.array([np.linalg.norm(closest_state) / np.sqrt(8)]),
-                'dist_closest_st' : np.array([np.linalg.norm(closest_tar) / np.sqrt(8)]),
+                'phi_closest_st' : phi_closest_st / (2*np.pi),
+                'phi_closest_tar' : phi_closest_tar / (2*np.pi),
+                'dist_closest_st' : dist_closest_st, 
+                'dist_closest_tar' : dist_closest_tar, 
                 'inside_area' : np.array([int(inside_area)]),
                 'area_full' : np.array([int(area_full)]) if inside_area else np.array([0.]),
                 'own_state' : own_state}#+ np.random.randn() * 0.05}
