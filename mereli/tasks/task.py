cc7211e5847224d4898b8caa7463807d206e8bca
r-sendra <r.sendra@alumnos.upm.es>
2022-10-12 15:22:47 +0200

    New reward function formation v3

--- mereli/tasks/task.py
+++ mereli/tasks/task.py
@@ -197,7 +197,7 @@ class CommFormation(Task):
         dist_tar = np.linalg.norm(my_state - closest) 
         
         if dist_neigh < self.threshold:
-            return 0.0
+            return - (1 - dist_neigh / self.threshold) 
         else:
             return max(0, 1 - dist_tar/self.threshold)
             # return np.exp(-alpha * dist_tar) 
