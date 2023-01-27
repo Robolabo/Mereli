98146ba7dbd7f68eeda37a80c03de1fb7b45ca15
r-sendra <r.sendra@alumnos.upm.es>
2023-01-27 19:04:59 +0100

    RBF exp: removed prints from debugging.

--- mereli/sensors/stateful_comm_rx.py
+++ mereli/sensors/stateful_comm_rx.py
@@ -117,7 +117,6 @@ class NewOrientStatefulCommRX(Sensor):
         # __import__('pdb').set_trace()
         dist_landmarks = np.mean([[np.exp(-2*dist_fn(lmark, st)**2) for lmark in self.landmarks] for st in neigh_states], axis=0)
         own_dist_lmarks = np.array([np.exp(-2*dist_fn(lmark, self.state)**2) for lmark in self.landmarks])
-        print(own_dist_lmarks, self.landmarks)
         self.t += 1
         return {'neigh_dist_lmarks' : dist_landmarks,# + np.random.randn(self.state_dim) * 0.05,
                 'own_dist_lmarks' : own_dist_lmarks,#  + np.random.randn(self.state_dim) * 0.05,
