2e5c147994a699ccb03c87f509ed299d57b65cff
r-sendra <r.sendra@alumnos.upm.es>
2023-09-11 11:44:10 +0200

    Reward fn was too severe. Fixed.

--- mereli/tasks/task.py
+++ mereli/tasks/task.py
@@ -225,6 +225,7 @@ class CommFormation(Task):
         dist_clst_lmark = entities[robot_name].virtual_particle.dist_clst_lmark
         if dist_clst_st is None or dist_clst_lmark is None:
             return 0.0
+        # if dist_clst_st < self.threshold or dist_clst_lmark > self.threshold:
         if dist_clst_st < self.threshold or dist_clst_lmark > self.threshold:
             return 0.0 # - (1 - dist_neigh / self.threshold) 
         else:
