808b3a0cd705a60173f87f5a66f8d3065c25f098
r-sendra <r.sendra@alumnos.upm.es>
2024-04-23 12:55:59 +0200

    Minor changes

--- mereli/controllers/formation.py
+++ mereli/controllers/formation.py
@@ -6,7 +6,6 @@ from mereli.utils import compute_angle, angle_diff
 from mereli.communication import RobotMolecule
 
 
-
 class EllipseFormation:
     def __init__(self, num_points, a=1, b=1):
         self._num_points = num_points 
@@ -52,35 +51,41 @@ class EllipseFormation:
         self._b = new_b
         self.compute_points()
 
-
-
-
 @controller_registry(name='ellipse_formation') 
 class EllipseFormationController(RobotController):
-    """ 
+    """ Robot controller for the ellipse formation using virtual spaces. 
     """
     def __init__(self, *args,  **kwargs):
         super(EllipseFormationController, self).__init__(*args, **kwargs)
         self.formation_name = 'ellipse' 
-        self.formation = EllipseFormation(16, a=1, b=1)
+        self.formation = EllipseFormation(8, a=1, b=1)
         self.flag = False
         self.init_center = None
 
     def plan_navigation(self, center):
-        """ Only controlling the CoM estimate """
+        """ Computes the points in a ellipse formation based on a given center.
+        The paramters of the ellipse are obtained via the formation attribute that contains an 
+        instance of the class EllipseFormation.
+
+        If the formation remains fixed during all the simulation then this method can be empty. 
+        In case of a coordinated movement keeping a given formation, then the center of mass can be 
+        moved using a given trajectory.
+        """
         com_eps = 0.2
     
         if self.t < 700: # Still converging to formation
             self.init_center = center # No problem with odom
         else:
+            pass
+            # IMPORTANT: IGNORE COMMENTS, IS FOR SOMETHING ELSE.
             """ DOOR """
-            if np.abs(center[1]) > 0.05:
-                center[1] = 0
-            center[0] += com_eps
-            if center[0] > 1.5 and center[0] < 5:
-                self.formation.b = max(0.15, self.formation.b - 0.05) 
-            elif center[0] > 5:
-                self.formation.b = min(1, self.formation.b + 0.05) 
+            # if np.abs(center[1]) > 0.05:
+            #     center[1] = 0
+            # center[0] += com_eps
+            # if center[0] > 1.5 and center[0] < 5:
+            #     self.formation.b = max(0.15, self.formation.b - 0.05) 
+            # elif center[0] > 5:
+            #     self.formation.b = min(1, self.formation.b + 0.05) 
 
             """ Ellipse oscill. excentriciy """
             # self.formation.b = 0.5  * (1 + np.cos(2*np.pi * .02 *  self.t * 0.05)) + 0.15
@@ -98,38 +103,41 @@ class EllipseFormationController(RobotController):
             # self.formation.a = 0.4
             # center[0] -= 0.1
             # center[1] = np.sin(2*np.pi*0.2*center[0]- self.init_center[0])
-
-
-
-
         return center
 
     def select_coords_lmark_formation(self):
-        # import matplotlib.pyplot as plt
+        """ The controller has access to the current landmark in the virtual space"""
         lmark = self.controller_owner.virtual_particle.lmark
 
         if lmark is None:
             self.target_coords = np.zeros(2)
             return
         state = self.controller_owner.virtual_particle.state
-
+        """ In mereli neighbors are computed external to the controller. """
         neigh_positions = np.vstack([epk.position[:2] for epk in self.controller_owner.neighbors])
+        # Estimate center of formation (in ellipse)
         center = np.mean(neigh_positions,0)
         center = self.plan_navigation(center)
-
         
+        # Compute the target position in the formation (in the real world) where the robot where the 
+        # robot should go. This coords are obtained based on the landmark and the center of mass. 
         self.target_coords = self.formation.coords[lmark].copy() if lmark is not None else np.zeros(2)
         self.target_coords += center
-        return 
 
 
     def step(self, state, reward=0.0):
+        """ Step method of the controller. 
+        Remark:  the state input argument is currently useless, sensors are read using get_sensor_reading (see below). 
+        In the future after a code clean the state arg will be removed. 
+        """
+        # Reading of GPS sensor
         curr_pos = self.get_sensor_reading('own_position_sensor')[:2]
+        # Important: Selection of the formation coordinates based on virtual space. 
         self.select_coords_lmark_formation()
 
+        # This part is not relevant for Nautilus, essentially it computes some simplified IK to 
+        # move the robot towards the target position. 
         dist_tar = np.linalg.norm(self.target_coords - curr_pos)
-        # if dist_tar > 10:
-        #     __import__('pdb').set_trace()
         desired_dir = (self.target_coords - curr_pos) / dist_tar
         # desired_dir -= v_obstacle
 
@@ -139,7 +147,6 @@ class EllipseFormationController(RobotController):
         a1 = compute_angle(desired_dir)
         a2 = compute_angle(heading_ori)
         alp = 5 # if len(self.formation.get('nodes', [])) <= 9 else 1
-        # A = 1 / (1 + np.exp(-alp * (dist_tar - .05)))
         A = 1 / (1 + np.exp(-5* (dist_tar - .6)))
         if dist_tar<= 0.05: A = 0
         # if dist_tar < 0.2:
@@ -161,17 +168,16 @@ class EllipseFormationController(RobotController):
                 action =  .2* np.array([1., -1])
             else:
                 action =  .2*np.array([-1., 1])
-
-        
-        # if np.linalg.norm(self.target_coords - curr_pos) < 0.1:
-        #     action = np.array([0,0])
         self.flag = True
         self.get_actuator('joint_velocity_actuator').action = action
 
     def reset(self):
         self.flag = False
+        # In this example all priorities to one. 
         self.robot.virtual_particle.lmark_priorities = np.ones(len(self.robot.virtual_particle.landmarks)) 
-        
+
+
+
 @controller_registry(name='ellipse_navigation') 
 class EllipseNavigationController(RobotController):
     """ 
